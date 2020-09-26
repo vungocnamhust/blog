2ce2c3343e358d247c5ad4dc89c42f946234274f
Nam Vu <vungocnam0409@gmail.com>
2020-09-26 15:29:58 +0700

    Add security (permission) for comment and create article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController 
+    http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
 
     def destroy
         @article = Article.find(params[:id])
