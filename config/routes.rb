bdd008388c7f524f82536f71eb80895a70e9f66c
Nam Vu <vungocnam0409@gmail.com>
2020-09-26 11:47:03 +0700

    Bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   get 'welcome/index'  # reference to controller Welcome, action index 
-  post 'articles/new',to:'articles#create'
-  get 'articles/new'
-  get 'articles/show',to:'articles#show',as: :articlesShow
-  get 'articles/index',to:'articles#index',as: :articleIndex
-  resources :articles
- 
+  # post 'articles/new',to:'articles#create'
+  # get 'articles/new'
+  # get 'articles/show',to:'articles#show',as: :articlesShow
+  # get 'articles/index',to:'articles#index',as: :articleIndex
+  resources :articles do
+    resources :comments
+  end
   root 'welcome#index'
 end
--- db/migrate/20200926024437_create_comments.rb
+++ db/migrate/20200926024437_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
-      t.string :comment
+      t.string :commenter
       t.text :body
       t.references :article, null: false, foreign_key: true
 
@@ -9,3 +9,6 @@ class CreateComments < ActiveRecord::Migration[6.0]
     end
   end
 end
+
+
+schema_migrations
