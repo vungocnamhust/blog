96efed2bfdbf8188d5b30dd7a22904f655f5713e
Nam Vu <vungocnam0409@gmail.com>
2020-09-26 09:43:43 +0700

    Add feature: delete, update method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,28 +1,45 @@
 class ArticlesController < ApplicationController 
 
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+
+        redirect_to articles_path
+    end
+
+    def update 
+        @article = Article.find(params[:id])
+        if (@article.update(article_params)) 
+            redirect_to @article
+        else 
+            render 'edit'
+        end
+    end
+
     def index 
         @articles = Article.all
     end
 
     def show
         @article = Article.find(params[:id])
+    end
 
-      end
     def new
-
         @email = "vungocnam0409@gmail.com"
+    end
+
+    def edit 
+        @article = Article.find(params[:id])
     end
 
     def create
         @article = Article.new(article_params)
-       
         if @article.save
             redirect_to articlesShow_url(id: @article.id)
         else 
             render 'new'
         end
-
-      end
+    end
        
       private
         def article_params
